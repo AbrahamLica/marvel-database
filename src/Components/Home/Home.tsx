5c1288f8f8ea428c62d439af354fabfe708b3d76
Abraham Melquisedeque Pereira Licá <103894033+AbrahamLica@users.noreply.github.com>
2023-06-18 05:50:05 -0300

    fixed banner images
    ...

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,27 +1,26 @@
-import React from "react";
 import * as C from "./AppStyles";
 import logo from "../../Media/logo.png";
 import { useEffect, useState } from "react";
 import bannercharacters from "../../Media/characters.png";
 import bannercomics from "../../Media/comics.png";
+import bannercreators from "../../Media/creators.png";
+import bannerevents from "../../Media/events.png";
+import bannerseries from "../../Media/series.png";
+import bannerstories from "../../Media/stories.png";
 
 const Home = () => {
   const [isHoverText, setIsHoverText] = useState(false);
   const [isHoverBanner, setIsHoverBanner] = useState(false);
-  const [bannerCharacters, setBannerCharacters] = useState(false);
-  const [bannerComics, setBannerComics] = useState();
-  // const [bannerCharacters, setCategoryName] = useState<any>();
-  // const [bannerCharacters, setCategoryName] = useState<any>();
-  // const [bannerCharacters, setCategoryName] = useState<any>();
-  // const [bannerCharacters, setCategoryName] = useState<any>();
+  const [banner, setBanner] = useState<string>();
+  const [bannerajustado, setBannerajustado] = useState<string>();
 
   useEffect(() => {
-  }, []);
+    teste();
+  }, [banner]);
 
-
-  function handleMouseOverText() {
+  function handleMouseOverText(banner: string) {
     setIsHoverText(true);
-    setBannerCharacters(true);
+    setBanner(banner);
   }
 
   function handleMouseOutText() {
@@ -40,13 +39,29 @@ const Home = () => {
     setIsHoverText(false);
   }
 
+  function teste() {
+    if (banner == "characters") {
+      return setBannerajustado(bannercharacters);
+    } else if (banner == "comics") {
+      return setBannerajustado(bannercomics);
+    } else if (banner == "creators") {
+      return setBannerajustado(bannercreators);
+    } else if (banner == "events") {
+      return setBannerajustado(bannerevents);
+    } else if (banner == "series") {
+      return setBannerajustado(bannerseries);
+    } else if (banner == "stories") {
+      return setBannerajustado(bannerstories);
+    }
+  }
+
   return (
     <C.MainContainer>
       <C.Header>
         <C.Logo src={logo} />
         <C.SubHeader>
           <C.CategoryText
-            onMouseOver={handleMouseOverText}
+            onMouseOver={() => handleMouseOverText("characters")}
             onMouseOut={handleMouseOutText}
           >
             Characters
@@ -59,30 +74,47 @@ const Home = () => {
               height: isHoverText || isHoverBanner ? "75vh" : "0%",
             }}
           >
-            <C.ImgBanner
-              {}        
-            ></C.ImgBanner>
+            <C.ImgBanner src={bannerajustado}></C.ImgBanner>
           </C.Banner>
 
           <C.CategoryText
-            onMouseOver={handleMouseOverText}
+            onMouseOver={() => handleMouseOverText("comics")}
             onMouseOut={handleMouseOutText}
           >
             Comics
           </C.CategoryText>
 
-          <C.CategoryText>Creators</C.CategoryText>
+          <C.CategoryText
+            onMouseOver={() => handleMouseOverText("creators")}
+            onMouseOut={handleMouseOutText}
+          >
+            Creators
+          </C.CategoryText>
           <C.Banner></C.Banner>
 
-          <C.CategoryText>Events</C.CategoryText>
+          <C.CategoryText
+            onMouseOver={() => handleMouseOverText("events")}
+            onMouseOut={handleMouseOutText}
+          >
+            Events
+          </C.CategoryText>
 
-          <C.CategoryText>Series</C.CategoryText>
+          <C.CategoryText
+            onMouseOver={() => handleMouseOverText("series")}
+            onMouseOut={handleMouseOutText}
+          >
+            Series
+          </C.CategoryText>
 
-          <C.CategoryText>Stories</C.CategoryText>
+          <C.CategoryText
+            onMouseOver={() => handleMouseOverText("stories")}
+            onMouseOut={handleMouseOutText}
+          >
+            Stories
+          </C.CategoryText>
         </C.SubHeader>
       </C.Header>
 
-      {/* <button onClick={teste2}>teste</button> */}
     </C.MainContainer>
   );
 };
